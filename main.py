21e9da1a7cfe87f766050df2c45da815de965e37
dhokoon <52174999+dhokoon@users.noreply.github.com>
2022-03-19 00:01:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+random_side = random.randint(0, 10)
+
+if random_side == 1:
+  print("Heads")
+else:
+  print("Tails")
+
+
+
+
+
+
+
+
+
+
